542076487411ae5835512fe917a307650641417f
Bumhan Yu <bumhan.yu@gmail.com>
2022-02-15 00:46:06 -0500

    add realtime database init

--- components/firebase/useUser.ts
+++ components/firebase/useUser.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import 'firebase/compat/database';
 import initFirebase from './initFirebase';
 import {
   removeUserCookie,
@@ -8,10 +9,12 @@ import {
   getUserFromCookie,
 } from './userCookies';
 import { mapUserData, UserInfo } from './mapUserData';
+
 initFirebase();
 
 const useUser = () => {
   const [user, setUser] = useState<UserInfo>();
+  const [db, setDb] = useState<any>();
   const logout = async () => {
     return firebase
       .auth()
@@ -24,7 +27,7 @@ const useUser = () => {
       .onIdTokenChanged((currentUser) => {
         if (currentUser) {
           const userData = mapUserData(currentUser);
-          console.log('user:', currentUser, 'userData', userData);
+          // console.log('user:', currentUser, 'userData', userData);
           setUserCookie(userData);
           setUser(userData);
         } else {
@@ -35,14 +38,23 @@ const useUser = () => {
 
     const userFromCookie = getUserFromCookie();
     if (!userFromCookie) return;
+
     setUser(userFromCookie);
 
     return () => {
       cancelAuthListener();
     };
   }, []);
-
-  return { user, logout };
+  useEffect(() => {
+    if (!user) return;
+    const database = firebase.database();
+    const foods = database.ref(`users/${user.id}/foods`);
+    setDb(database);
+    foods.on('value', (data) => {
+      console.log(data.val());
+    });
+  }, [user]);
+  return { user, logout, db };
 };
 
 export { useUser };
